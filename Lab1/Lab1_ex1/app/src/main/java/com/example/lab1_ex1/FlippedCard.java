9f7fbcf99d7595e8be3eab5ae21e9e9dc36e8eeb
amaanaz <aazubairi@hotmail.com>
2022-02-19 17:42:46 +0300

    SE328L Mob Dev Lab 1

---
+++ Lab1/Lab1_ex1/app/src/main/java/com/example/lab1_ex1/FlippedCard.java
@@ -0,0 +1,13 @@
+package com.example.lab1_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlippedCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flipped_card);
+    }
+}
